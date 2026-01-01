a42da5b37b7951396bddaa910bc4761d73fa28fa
ashmeet07 <ashmeetsingh0123@gmail.com>
2026-01-02 01:26:56 +0530

    first two basic completed

---
+++ src/arraytraverse.cpp
@@ -0,0 +1,24 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int a[]={1,2,3,4,5,91,19,1929,22,11};
+    
+    int size=sizeof(a)/sizeof(a[0]);
+    for (int i=0; i<size;i++)
+        cout<<a[i]<<" ,";
+    cout<<endl;
+    for (int i : a)
+        cout<<i<<" ,";
+        
+    cout<<endl;
+    int i=0;
+    while(i<size){
+        cout<<a[i]<<" ,";
+        i+=1;
+    }
+ 
+    cout<<endl;
+    
+    return 0;
+}
---
+++ src/printhelloworld.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"First file to start the DSA with !";
+
+    return 1-1;
+}
