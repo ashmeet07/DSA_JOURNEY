d14c31ebaf1d6b141a655089e5a4ef4520863288
ashmeet07 <ashmeetsingh0123@gmail.com>
2026-01-02 21:22:12 +0530

    update

---
+++ src/triangle6.cpp
@@ -0,0 +1,15 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the no of rows";
+    cin>>n;
+   for(int i=0;i<n;i++){
+        for (int j=0;j<=i-1;j++)
+            cout<<"  ";
+        for (int k=0;k<=2*(n-i-1);k++)
+            cout<<"* ";
+        cout<<endl;
+   }
+}
---
+++ src/triangle7.cpp
@@ -0,0 +1,23 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter no of rows you wan to print the diamond";
+    cin>>n;
+  for (int i=0;i<n;i++){
+    for (int j=0;j<n-i;j++)
+        cout<<" ";
+    for(int k=0;k<i*2-1;k++)
+        cout<<"*";
+    cout<<endl;
+    }
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++)
+            cout<<" ";
+        for(int k=0;k<2*(n-i)-1;k++)
+            cout<<"*";
+        cout<<endl;
+    }
+    return 0;
+}
